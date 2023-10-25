5403822eb8aac5c550966c7c03d878e96b8f9b34
feketeoliver <feketeoliver2006@gmail.com>
2023-10-25 14:17:50 +0200

    nig

---
+++ script.js
@@ -0,0 +1,12 @@
+const content = document.querySelector(".content");
+for(let i = 0; i < 64; i++){
+    //Template Literal Alrgr+7 - ${változó}
+    content.innerHTML += `<div class="box" id="${i+1}"></div>`;
+}
+//NodeList-ként megkapjuk a .box-okat:
+const boxes = document.querySelector(".box");
+boxes.forEach(element => {
+    element.addEventListener("click", (event) => {
+        event.target.style.backgorundColor = "#ff0000";
+   }) 
+})
