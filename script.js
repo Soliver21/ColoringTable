2eb2b34b2d5cadbb6ffcd03d3a1e195128d2e256
feketeoliver <feketeoliver2006@gmail.com>
2023-11-07 11:29:31 +0100

    kaki

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-const content = document.querySelector(".content");
+const content = document.querySelector(".content")
 for(let i = 0; i < 64; i++){
     //Template Literal Alrgr+7 - ${változó}
-    content.innerHTML += `<div class="box" id="${i+1}"></div>`;
+    content.innerHTML += `<div class="box" id= "${i+1}"></div>`;
 }
 //NodeList-ként megkapjuk a .box-okat:
-const boxes = document.querySelector(".box");
+const boxes = document.querySelectorAll(".box");
 boxes.forEach(element => {
     element.addEventListener("click", (event) => {
-        event.target.style.backgroundColor = "#ff0000";
-   }) 
+        let currentElement = event.target;
+        if (currentElement.style.backgroundColor === ""){
+            currentElement.style.backgroundColor = "#ff0000";
+            currentElement.textContent = event.target.id;
+        }
+        else{
+            currentElement.style.backgroundColor = "";
+            currentElement.textContent = null;
+        }
+        currentElement.style.textAlign = "center";
+   })
 })
